726c9fcd0a92b513e5fb42e84fe2d75b1ce0e166
Adrian Muntean <m.adr.home@gmail.com>
2018-05-23 23:20:34 +0300

    Add rotating ramp

---
+++ Assets/Scripts/RotateRamp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRamp : MonoBehaviour {
+
+    private const int UP = 1;
+    private const int DOWN = 0;
+    private const float rotateSpeed = 10; 
+    private int moveDirection = DOWN;
+
+	// Update is called once per frame
+	void Update () {
+        if (moveDirection == UP)
+        {
+            transform.Rotate(new Vector3(rotateSpeed, 0, 0) * Time.deltaTime);
+            if (isUp())
+            {
+                moveDirection = DOWN;
+            }
+        }
+
+        if (moveDirection == DOWN)
+        {
+            transform.Rotate(new Vector3(-rotateSpeed, 0, 0) * Time.deltaTime);
+            if (isDown())
+            {
+                moveDirection = UP;
+            }
+        }
+    }
+
+    private bool isDown()
+    {
+        return transform.rotation.x <= -60;
+    }
+
+    private bool isUp()
+    {
+        return transform.rotation.x <= -120;
+    }
+}
